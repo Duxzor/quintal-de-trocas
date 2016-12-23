8cfd526de4a0c02e4485362b6901d88522f28274
Diego Dillenburg Bueno <diegodillenburg@gmail.com>
2016-12-23 15:17:55 -0200

    Ajustes finais no deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :repo_url, 'git@github.com:Duxzor/quintal-de-trocas'
 set :branch, :master
 set :deploy_to, '/home/ubuntu/quintal'
 set :pty, true
-set :linked_files, %w{config/database.yml public/robots.txt}
+set :linked_files, %w{config/database.yml config/application.yml public/robots.txt}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
 set :rvm_type, :user
@@ -14,7 +14,7 @@ set :rvm_ruby_version, 'ruby-2.3.1'
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/quintal-puma.sock"    #accept array for multi-bind
 set :puma_conf, "#{shared_path}/puma.rb"
 set :puma_access_log, "#{shared_path}/log/puma_error.log"
 set :puma_error_log, "#{shared_path}/log/puma_access.log"
